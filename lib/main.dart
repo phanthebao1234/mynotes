04c0b3a6eb80b232defbf976fbbe5e4b02fb0a61
phanthebao1234 <phanthebao888@gmail.com>
2024-03-02 22:52:19 +0700

    step-22

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_service.dart';
 import 'package:mynotes/views/login_view.dart';
@@ -32,33 +34,176 @@ void main() {
 
 enum MenuAction { logout, settings }
 
-class HomePage extends StatelessWidget {
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//         // trong FutureBuiler sẽ có 2 thằng là future và builder
+//         // Trong đó futture: Tính toán bất đồng bộ mà trình tạo này hiện đang kết nối, có thể là rỗng. => nói chung là xử lý bất đồng bộ
+//         // TOD      builder: Chiến lược xây dựng hiện đang được người xây dựng này sử dụng. => Code giao diện thì đi vô đây
+//         future: AuthService.firebase().initialize(),
+//         builder: (context, snapshot) {
+//           switch (snapshot.connectionState) {
+//             case ConnectionState.done:
+//               final user = AuthService.firebase().currentUser;
+//               devtools.log(user.toString());
+//               if (user != null) {
+//                 if (user.isEmailVerified) {
+//                   return const NotesView();
+//                 } else {
+//                   return const VerifyEmailView();
+//                 }
+//               } else {
+//                 return const LoginView();
+//               }
+//             default:
+//               return const CircularProgressIndicator();
+//           }
+//         });
+//   }
+// }
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        // trong FutureBuiler sẽ có 2 thằng là future và builder
-        // Trong đó futture: Tính toán bất đồng bộ mà trình tạo này hiện đang kết nối, có thể là rỗng. => nói chung là xử lý bất đồng bộ
-        // TOD      builder: Chiến lược xây dựng hiện đang được người xây dựng này sử dụng. => Code giao diện thì đi vô đây
-        future: AuthService.firebase().initialize(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = AuthService.firebase().currentUser;
-              devtools.log(user.toString());
-              if (user != null) {
-                if (user.isEmailVerified) {
-                  return const NotesView();
-                } else {
-                  return const VerifyEmailView();
-                }
-              } else {
-                return const LoginView();
-              }
-            default:
-              return const CircularProgressIndicator();
-          }
-        });
+    return BlocProvider(
+      create: (context) => CouterBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello world"),
+        ),
+        body: BlocConsumer<CouterBloc, CouterState>(
+          builder: (context, state) {
+            final invalidValue =
+                (state is CouterStateInvalidNumber ? state.invalidValue : '');
+            return Column(
+              children: [
+                Text("Current value => ${state.value}"),
+                Visibility(
+                  visible: state is CouterStateInvalidNumber,
+                  child: Text("Invalid input => ${state.value}"),
+                ),
+                TextField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter input here ...',
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+                Row(
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        context
+                            .read<CouterBloc>()
+                            .add(IncrementEvent(_controller.text));
+                      },
+                      child: const Text('+'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        context
+                            .read<CouterBloc>()
+                            .add(DecrementEvent(_controller.text));
+                      },
+                      child: const Text('-'),
+                    )
+                  ],
+                )
+              ],
+            );
+          },
+          listener: ((context, state) {
+            _controller.clear();
+          }),
+        ),
+      ),
+    );
+  }
+}
+
+@immutable
+abstract class CouterState {
+  final int value;
+  const CouterState(this.value);
+}
+
+class CouterStateValid extends CouterState {
+  const CouterStateValid(super.value);
+}
+
+class CouterStateInvalidNumber extends CouterState {
+  final String invalidValue;
+  const CouterStateInvalidNumber({
+    required this.invalidValue,
+    required int previousValue,
+  }) : super(previousValue);
+}
+
+@immutable
+abstract class CouterEvent {
+  final String value;
+  const CouterEvent(this.value);
+}
+
+class IncrementEvent extends CouterEvent {
+  const IncrementEvent(super.value);
+}
+
+class DecrementEvent extends CouterEvent {
+  const DecrementEvent(super.value);
+}
+
+class CouterBloc extends Bloc<CouterEvent, CouterState> {
+  CouterBloc() : super(const CouterStateValid(0)) {
+    on<IncrementEvent>((event, emit) {
+      final interger = int.tryParse(event.value);
+      if (interger == null) {
+        emit(
+          CouterStateInvalidNumber(
+            invalidValue: event.value,
+            previousValue: state.value,
+          ),
+        );
+      } else {
+        emit(CouterStateValid(state.value + interger));
+      }
+    });
+    on<DecrementEvent>((event, emit) {
+      final interger = int.tryParse(event.value);
+      if (interger == null) {
+        emit(
+          CouterStateInvalidNumber(
+            invalidValue: event.value,
+            previousValue: state.value,
+          ),
+        );
+      } else {
+        emit(CouterStateValid(state.value - interger));
+      }
+    });
   }
 }
